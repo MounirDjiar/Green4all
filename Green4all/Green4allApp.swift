05961f31af6a1ec6f66baf078c63647413d94174
SPIRIIT-hub <baptistemoulin18@gmail.com>
2021-03-18 09:38:23 +0100

    init

---
+++ Green4all/Green4allApp.swift
@@ -0,0 +1,17 @@
+//
+//  Green4allApp.swift
+//  Green4all
+//
+//  Created by Baptiste Moulin on 18/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Green4allApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

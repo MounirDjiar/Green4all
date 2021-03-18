96f840e497f13389caddf516954be784d84a9849
yokomotoh <yokomotohama@hotmail.com>
2021-03-18 10:13:45 +0100

    Market folder and Market.swift are created.

---
+++ Green4all/Market/Market.swift
@@ -0,0 +1,20 @@
+//
+//  Market.swift
+//  Green4all
+//
+//  Created by vincent schmitt on 18/03/2021.
+//
+
+import SwiftUI
+
+struct Market: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Market_Previews: PreviewProvider {
+    static var previews: some View {
+        Market()
+    }
+}

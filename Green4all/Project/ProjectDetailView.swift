8d5e1274a427b1157e9bd3281e2bdcd926a72235
MounirDjiar <mounir@djiar.com>
2021-03-18 10:08:54 +0100

    Init branch Mounir

---
+++ Green4all/Project/ProjectDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectDetailView.swift
+//  Green4all
+//
+//  Created by Mounir DJIAR on 18/03/2021.
+//
+
+import SwiftUI
+
+struct ProjectDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProjectDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectDetailView()
+    }
+}
---
+++ Green4all/Project/ProjectListView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectListView.swift
+//  Green4all
+//
+//  Created by Mounir DJIAR on 18/03/2021.
+//
+
+import SwiftUI
+
+struct ProjectListView: View {
+    var body: some View {
+        Text("Branch Mounir")
+    }
+}
+
+struct ProjectListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectListView()
+    }
+}

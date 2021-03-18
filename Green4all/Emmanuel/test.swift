220da9c438f3cd24985c80ac55658c0fc3ad82fa
emm <723125@gmail.com>
2021-03-18 09:08:30 +0000

    test

---
+++ Green4all/Emmanuel/test.swift
@@ -0,0 +1,20 @@
+//
+//  test.swift
+//  Green4all
+//
+//  Created by emm on 18/03/2021.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct test_Previews: PreviewProvider {
+    static var previews: some View {
+        test()
+    }
+}

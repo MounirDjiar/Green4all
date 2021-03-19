b2c679116163da10d27a2ebf2dfb5c14d46a39d6
bashalir <bachir@sahali.fr>
2021-03-19 11:51:10 +0100

    Fix ProfilConnexion View

--- Green4all/Profil/ProfilConnexion.swift
+++ Green4all/Profil/ProfilConnexion.swift
@@ -20,35 +20,62 @@ struct ProfilConnexion: View {
             ZStack{
                 Color("bgGreen")
                     .ignoresSafeArea()
-                    
+                
                 
                 VStack{
-                Form {
-                    TextField("", text: $mail)
-                        .onAppear{
-                            UITextField.appearance().attributedPlaceholder = NSAttributedString(string: "Émail", attributes: [NSAttributedString.Key.foregroundColor: UIColor.green])
-                        }
-                    SecureField("Mot de Passe", text: $password)
+                    Form {
+                        Section {
+                        TextField("Émail", text: $mail)
+                            .onAppear{
+                                UITextField.appearance().attributedPlaceholder = NSAttributedString(string: "Émail", attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
+                            }
+                        SecureField("Mot de Passe", text: $password)
                         
+                        }
+                    }
+                    .foregroundColor(Color("bgDarkGreen"))
+                    .frame(maxHeight: 150)
+                    .onAppear{
+                        UITableView.appearance().backgroundColor = .clear
+                    }
+                    
+                    VStack(spacing: 50){
+                        
+                        VStack{
+                        Button(action: {}, label: {
+                            Text("Se Connecter")
+                                .fontWeight(.bold)
+                        })
+                        .padding(.horizontal, 100)
+                        .padding()
+                        .background(Color("bgDarkGreen"))
+                        .cornerRadius(10)
+                        
+                        
+                        Button(action: {}, label: {
+                            Text("Mot de Passe oublié ?")
+                                .fontWeight(.bold)
+                        })
+                        }
+                        
+                        Divider()
+                        
+                        VStack(spacing:15){
+                        Text("Nouveau sur Green4All ?")
+                            .fontWeight(.bold)
+                        
+                        Button(action: {}, label: {
+                            Text("S'inscrire")
+                                .fontWeight(.bold)
+                        })
+                        .padding(.horizontal, 115)
+                        .padding()
+                        .background(Color("bgDarkGreen"))
+                        .cornerRadius(10)
+                        }
+                    }
+                    
                 }
-                .onAppear{
-                    UITableView.appearance().backgroundColor = .clear
-                }
-                   
-
-                }
-                
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Text("Se Connecter")
-                })
-                .padding(.horizontal, 100)
-                .padding()
-                .background(Color.green)
-                .cornerRadius(10)
-                
-                Spacer()
-                
-                
                 
             }.foregroundColor(.white)
             .navigationBarTitle("Connexion", displayMode: .inline)
@@ -57,8 +84,8 @@ struct ProfilConnexion: View {
                 UINavigationBar.appearance().barTintColor = .clear
                 UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
                 UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
-            
-    }
+                
+            }
         }
 
     }}
